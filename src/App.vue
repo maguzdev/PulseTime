2f22dc1c11197258af7aa7de418a9c08b4feedb8
Manuel Guzman <maguz727@gmail.com>
2025-11-25 10:29:51 -0500

    Agregar componente Hero y actualizar App.vue para incluirlo

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup>
 	// Componentes
 	import Header from "@/components/Header.vue";
+	import Hero from "@/components/Hero.vue";
 </script>
 
 <template>
 	<div>
+		<!-- Header -->
 		<Header />
+		<!-- Hero Section -->
+		<Hero />
 	</div>
 </template>
 
---
+++ src/components/Hero.vue
@@ -0,0 +1,20 @@
+<template>
+	<section>
+		<div
+			class="bg-white flex flex-col h-80 shadow-lg justify-center items-center text-center"
+		>
+			<h1
+				class="text-5xl font-bold bg-linear-to-r from-violet-400 via-purple-400 to-indigo-400 bg-clip-text text-transparent mb-4"
+			>
+				PulseTime
+			</h1>
+			<p class="text-xl text-gray-800 mb-8">
+				Gestiona tus tareas con elegancia y productividad
+			</p>
+			<p class="text-gray-700">
+				Tu aplicación definitiva para la gestión de tareas. Organiza,
+				prioriza y completa tus tareas diarias con facilidad.
+			</p>
+		</div>
+	</section>
+</template>
