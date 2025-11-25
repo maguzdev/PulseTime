b5385b2c821252b489218af913739d50cf5c00ec
Manuel Guzman <maguz727@gmail.com>
2025-11-25 17:47:46 -0500

    Agregar componente FormTask y actualizar App.vue para incluirlo

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
 	import Header from "@/components/Header.vue";
 	import Hero from "@/components/Hero.vue";
 	import TaskView from "@/components/TaskView.vue";
+	import FormTask from "@/components/FormTask.vue";
 
 	// Variable reactiva para almacenar las tareas
 	const tasksList = ref([]);
@@ -24,6 +25,8 @@
 		<Header />
 		<!-- Hero Section -->
 		<Hero />
+		<!-- Form Task Section (Temporal hasta que aplique las rutas)-->
+		<FormTask />
 		<!-- Task View Section (Temporal hasta que aplique las rutas)-->
 		<TaskView :tasksList="tasksList" />
 	</div>
